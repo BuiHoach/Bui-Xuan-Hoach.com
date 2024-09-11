e2c0ba1cb2359a8d9fd6b3ec1d5853c0dc212806
Buihoach <buithuong0812@gmail.com>
2024-09-11 15:43:51 +0700

    xuongmoc

--- css/photoshop.js
+++ css/photoshop.js
@@ -1,85 +1,82 @@
 
-$(document).ready(function () {
-  $('nav-icon').click(function () {
-    $(this).toggleclass('fa-solid');
-    $('nav-icon ').toggleclass('fa-solid')
-  });
-});
+
 
 
 
 
 $('#list-product').slick({
-  dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 1,
+  dots: false,               // Ẩn dấu chấm điều hướng
+  infinite: true,            // Vòng lặp vô hạn
+  speed: 300,                // Tốc độ chuyển slide (ms)
+  slidesToShow: 4,           // Hiển thị 4 sản phẩm cùng lúc
+  slidesToScroll: 1,         // Cuộn 1 sản phẩm mỗi lần
+  autoplay: true,            // Tự động chuyển slide
+  autoplaySpeed: 1000,       // Thời gian chuyển slide tự động (2 giây)
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+        slidesToShow: 3,     // Hiển thị 3 sản phẩm với màn hình nhỏ hơn 1024px
+        slidesToScroll: 3,   // Cuộn 3 sản phẩm mỗi lần
+        infinite: true,      // Vòng lặp vô hạn
+        dots: true           // Hiển thị dấu chấm điều hướng cho màn hình nhỏ
       }
     },
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 2,     // Hiển thị 2 sản phẩm với màn hình nhỏ hơn 600px
+        slidesToScroll: 2    // Cuộn 2 sản phẩm mỗi lần
       }
     },
     {
       breakpoint: 480,
       settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToShow: 1,     // Hiển thị 1 sản phẩm với màn hình nhỏ hơn 480px
+        slidesToScroll: 1    // Cuộn 1 sản phẩm mỗi lần
       }
     }
-
   ]
 });
+
 // 
 $('#list-product  .slick-arrow').html("");
 $('#list-product  .slick-arrow.slick-next').html("<span>></span>");
 $('#list-product  .slick-arrow.slick-prev').html("<span><</span>");
 $('#list-product  .slick-arrow.slick-prev').html("<span><</span>");
-
-$('#list-doi-tac').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 7,
-  slidesToScroll: 1,
-  arrows: false,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 3,
-        infinite: true,
+$(document).ready(function(){
+  $('#lists-doi-tac').slick({
+    dots: false,                // Ẩn các dấu chấm điều hướng
+    infinite: true,             // Vòng lặp vô hạn
+    speed: 300,                 // Tốc độ chuyển đổi (300ms)
+    slidesToShow: 6,            // Hiển thị 7 đối tác cùng lúc
+    slidesToScroll: 1,          // Cuộn 1 logo mỗi lần
+    autoplay: true,             // Tự động lướt
+    autoplaySpeed:1000,        // Chuyển mỗi 1 giây
+    arrows: Animation,               // Hiển thị mũi tên điều hướng
+    responsive: [
+      {
+        breakpoint: 1024,       // Với màn hình nhỏ hơn 1024px
+        settings: {
+          slidesToShow: 3,      // Hiển thị 3 đối tác
+          slidesToScroll: 1     // Cuộn 1 logo mỗi lần
+        }
+      },
+      {
+        breakpoint: 600,        // Với màn hình nhỏ hơn 600px
+        settings: {
+          slidesToShow: 2,      // Hiển thị 2 đối tác
+          slidesToScroll: 1     // Cuộn 1 logo mỗi lần
+        }
+      },
+      {
+        breakpoint: 480,        // Với màn hình nhỏ hơn 480px
+        settings: {
+          slidesToShow: 1,      // Hiển thị 1 đối tác
+          slidesToScroll: 1     // Cuộn 1 logo mỗi lần
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+    ]
+  });
 });
+
